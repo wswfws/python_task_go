e4791ab0225530c2b735361dea72604e3c265f80
Tatiana <tanya-sok-05@mail.ru>
2024-10-06 16:51:25 +0500

    test gogame_gui

--- test/test_GoGameGUI.py
+++ test/test_GoGameGUI.py
@@ -1,21 +1,21 @@
 import unittest
 import configuration as cfg
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 from go_game_gui import GoGameGUI
 
 
 class TestGoGameGUI(unittest.TestCase):
     def setUp(self):
         # Мок объекты для game_logic и surface
-        self.mock_game_logic = MagicMock()
+        self.mock_game_gui = MagicMock()
         self.mock_surface = MagicMock()
 
         # Параметры для game_logic и bordConfig
-        self.mock_game_logic.size = 9   # Размер доски 9x9
-        self.mock_game_logic.black_score = 10
-        self.mock_game_logic.white_score = 15
-        self.mock_game_logic.board = [
+        self.mock_game_gui.board_size = 9   # Размер доски 9x9
+        self.mock_game_gui.black_score = 10
+        self.mock_game_gui.white_score = 15
+        self.mock_game_gui.board = [
             ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
             ['.', 'B', '.', '.', '.', '.', '.', 'W', '.'],
             ['.', '.', '.', '.', 'B', '.', '.', '.', '.'],
@@ -29,12 +29,12 @@ class TestGoGameGUI(unittest.TestCase):
 
         # Мок для bordConfig
         self.mock_bordConfig = MagicMock()
-        self.mock_bordConfig.GRID_SIZE.return_value = 40
+        self.mock_bordConfig.GRID_CELL_SIZE.return_value = 40
         self.mock_bordConfig.STONE_RADIUS.return_value = 3
-        self.mock_game_logic.bordConfig = self.mock_bordConfig
+        self.mock_game_gui.bordConfig = self.mock_bordConfig
 
         # Экземпляр GUI с мок-объектами
-        self.gui = GoGameGUI(self.mock_game_logic, self.mock_surface)
+        self.gui = GoGameGUI(self.mock_game_gui, self.mock_surface)
 
     @patch('pygame.draw.line')
     @patch('pygame.draw.circle')
@@ -51,7 +51,7 @@ class TestGoGameGUI(unittest.TestCase):
         self.gui.draw()
 
         # Проверка вызовов для отрисовки линий
-        self.assertEqual(mock_draw_line.call_count, 2 * self.mock_game_logic.size)
+        self.assertEqual(mock_draw_line.call_count, 18)
 
         # Проверка вызовов для отрисовки камней
         self.assertEqual(mock_draw_circle.call_count, 4)  # Мы знаем, что на доске 4 камня
@@ -61,10 +61,10 @@ class TestGoGameGUI(unittest.TestCase):
         mock_font.render.assert_any_call('Белые: 15', True, cfg.TEXT_COLOR)
 
         # Проверка вызовов для отрисовки кнопки
-        mock_draw_rect.assert_called_once()
+        self.assertEqual(mock_draw_rect.call_count, 2)
 
         # Убедимся, что `blit` вызван для текстов очков и кнопки
-        self.assertGreaterEqual(self.mock_surface.blit.call_count, 3)  # Два для очков, один для кнопки
+        self.assertGreaterEqual(self.mock_surface.blit.call_count, 4)  # Два для очков, один для кнопки
 
     @patch('pygame.draw.line')
     def test_draw_lines(self, mock_draw_line):
@@ -122,7 +122,7 @@ class TestGoGameGUI(unittest.TestCase):
         half_grid = 20
         self.gui.draw_scores(half_grid)
 
-        # Вызван ли ендер текста для очков черных и белых
+        # Вызван ли pендер текста для очков черных и белых
         mock_font.render.assert_any_call('Черные: 10', True, cfg.TEXT_COLOR)
         mock_font.render.assert_any_call('Белые: 15', True, cfg.TEXT_COLOR)
 
@@ -139,14 +139,17 @@ class TestGoGameGUI(unittest.TestCase):
         half_grid = 20
         self.gui.draw_buttons(half_grid)
 
-        # Вызван ли метод отрисовки прямоугольника (кнопки)
-        mock_draw_rect.assert_called_once()
+        # Вызван ли метод отрисовки прямоугольников (кнопок)
+        self.assertEqual(mock_draw_rect.call_count, 2)
 
+        expected_calls = [
+            call("Пас", True, cfg.TEXT_COLOR),
+            call("?", True, cfg.TEXT_COLOR)
+        ]
         # Отрендерен ли текст для кнопки
-        mock_font.render.assert_called_once_with("Пас", True, cfg.TEXT_COLOR)
-
+        mock_font.render.has_calls(expected_calls, any_order=True)
         # Вызван ли blit для текста на кнопке
-        self.assertEqual(self.mock_surface.blit.call_count, 1)
+        self.assertEqual(self.mock_surface.blit.call_count, 2)
 
 
 if __name__ == '__main__':
